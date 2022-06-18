a7c2ba4807b08a2c054d351eae1beacf862eb866
noebernigaud <noe.bernigaud@etu.unice.fr>
2022-06-18 13:09:42 +0200

    zombie

--- src/game_events.rs
+++ src/game_events.rs
@@ -1,10 +1,20 @@
+use serde::{Deserialize, Serialize};
 use tokio::time::{sleep, Duration};
 use tungstenite::Message;
+
+#[derive(Serialize, Deserialize)]
+struct MonterData {
+    pos_x: f32,
+    pos_y: f32,
+    pos_z: f32,
+    username: String,
+    direction: String,
+}
 
 use crate::PeerMap;
 
 pub async fn game_events(peer_map: PeerMap) {
-    let mut hour = 12.0;
+    let mut hour = 16.0;
     loop {
         sleep(Duration::from_millis(1000)).await;
 
@@ -22,6 +32,29 @@ pub async fn game_events(peer_map: PeerMap) {
             let username_message = format!(r#" {{"route": "hour", "content": "{}"}} "#, hour);
             recp.unbounded_send(Message::from(username_message))
                 .unwrap();
+
+            if hour > 22.0 || hour < 7.0 {
+                let monster_data = MonterData {
+                    pos_x: 0.0,
+                    pos_y: 1.0099999439170835,
+                    pos_z: 0.0,
+                    username: String::from("zombie"),
+                    direction: String::from(
+                        r#" {\"_isDirty\":true,\"_x\":0.23749832808971405,\"_y\":0,\"_z\":0.9713879227638245} "#,
+                    ),
+                };
+                //let string_monster_data = serde_json::to_string(&monster_data).unwrap();
+                let new_monster_message = format!(
+                    r#" {{"route": "monster_data", "content": "{{\"pos_x\": {}, \"pos_y\": {}, \"pos_z\": {}, \"username\": \"{}\", \"direction\": {}}}"}} "#,
+                    monster_data.pos_x,
+                    monster_data.pos_y,
+                    monster_data.pos_z,
+                    monster_data.username,
+                    monster_data.direction
+                );
+                recp.unbounded_send(Message::from(new_monster_message))
+                    .unwrap();
+            }
         }
     }
 }
